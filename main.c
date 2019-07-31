3ada9106e8f187403ad01c914c6732d2d5373664
Sem Postma <sem.postma@gmail.com>
2019-07-31 14:49:54 +0200

    various

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "config.h"
+
+#include "CMemLeak.h"
+
+char t (int i) {
+    return i && !(i & (i - 1));
+}
+
+int main(int argc, char* argv[]) {
+    for(int i = 0; i < argc; i++) {
+        printf("%s\n", argv[i]);
+    }
+
+    #ifdef _DEBUG
+    XWBReport("Done");
+    #endif
+
+    getchar();
+}
+
+
+
+
+
+
+
