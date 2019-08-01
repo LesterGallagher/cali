8596e5746b794cd297ec3d2bf1dfa1d705a9c328
Sem Postma <sem.postma@gmail.com>
2019-08-01 14:40:44 +0200

    various

--- main.c
+++ main.c
@@ -2,11 +2,8 @@
 #include <stdio.h>
 
 #include "config.h"
-
 #include "CMemLeak.h"
-
-#include "list.h"
-#include "map.h"
+#include "hexdump.h"
 
 char t (int i) {
     return i && !(i & (i - 1));
@@ -17,32 +14,9 @@ int main(int argc, char* argv[]) {
         printf("%s\n", argv[i]);
     }
 
-	list(int) *v = list_new(int);
-
-    list_push(v, 42);
-    list_push(v, 64);
-
-    list_print(v);
-
-    hashmap_kvp(int);
-
-    hashmap(int) *m = hashmap_new(int);
-
-    char *key = "meaning of life";
-
-    struct hashmap_kvp_int *kvp;
-    
-    kvp = hashmap_get(m, key);
-
-    // hashmap_put(m, key, 42);
-
-    // hashmap_kvp(int) item = hashmap_get(m, "meaning of life");
-    
     #ifdef _DEBUG
     XWBReport("Done");
     #endif
-
-    getchar();
 }
 
 
